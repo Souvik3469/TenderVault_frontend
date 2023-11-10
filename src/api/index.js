40d1eba4ce7ccb1f066501e762e5ec3c45d4881a
Souvik3469 <souviksen3469@gmail.com>
2023-11-10 19:38:45 +0530

    backend url updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `http://localhost:5000/v1/`,
+  baseURL: `https://tendervault-backend.onrender.com/v1/`,
   headers: {
     "Content-Type": "application/json",
   },
